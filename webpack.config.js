34d65f6101989bb05c6a39308ede9075213f9931
Toily <escusado@gmail.com>
2016-12-17 00:13:19 -0600

    Fixes output

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     entry: './entry.js',
     output: {
-        path: __dirname + 'dist',
+        path: __dirname + '/dist',
         filename: 'bundle.js'
     },
     module: {
