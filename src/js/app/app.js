4ea1f97b87f8d06102b3410b17d00b7ea1a8850d
Toily <escusado@gmail.com>
2016-12-17 00:09:52 -0600

    Adds app scaffold

---
+++ src/js/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = class {
+  init () {
+    console.log('App init');
+  }
+
+  setup () {
+    console.log('App setup');
+  }
+
+  run () {
+    console.log('App run');
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log('>>>>');
+'use strict';
+
+const App = require('./app/app');
+
+var onDomReady = function onDomReady() {
+  window.app = new App();
+
+  app.setup();
+  app.run();
+};
+
+//on dom ready
+if (document.readyState != 'loading'){
+  onDomReady();
+} else {
+  document.addEventListener('DOMContentLoaded', onDomReady);
+}
